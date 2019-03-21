49257f9071f4fe81e8eb4e7de2c0c816c692c3cb
Alli Zembrodt <zembrodta3@nku.edu>
2019-03-20 21:16:17 -0400

    Character Part of App Updated

--- GameofThrones/app.R
+++ GameofThrones/app.R
@@ -17,11 +17,14 @@ library(ggthemes)
 # I would like to source this and put this code in a different file
 ScreenTime <- read_excel("~/Documents/GameOfThrones/ScreenTime.xlsx")
 houses <- read_csv("~/Documents/GitHub/GOT/houses.csv")
+Goodness<- read_excel("~/Documents/GameOfThrones/Goodness.xlsx")
 ScreenTime = left_join(ScreenTime, houses)
 top10 = head(ScreenTime, 10)
 top10melted = melt(top10, id.var = c("character", "House", "Color", "Episodes"))
 starks = c("Jon Snow", "Arya Stark", "Sansa Stark")
 lannister = c("Tyrion Lannister", "Cersei Lannister", "Jaime Lannister")
+ScreenTime1 = left_join(ScreenTime, Goodness)
+
 top10TimePerEpisode = top10melted %>%group_by(character) %>% summarise(timePerEp = sum(value)/ mean(Episodes))
 
 # Define UI for application that draws a histogram
@@ -32,50 +35,110 @@ ui <- fluidPage(
    titlePanel("Game of Thrones"),
    
    # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         selectInput("Character",
-                     label = "Choose Character to display their Screen Time",
-                     choices = c("Jon Snow", 
-                                 "Tyrion Lannister", 
-                                 "Daenerys Targaryen", 
-                                 "Sansa Stark", 
-                                 "Cersei Lannister", 
-                                 "Arya Stark", 
-                                 "Jaime Lannister", 
-                                 "Samwell Tarly"), 
-                     selected = "Jon Snow"),
-         
-         selectInput("Character2",
-                     label = "Choose Character to display their Screen Time",
-                     choices = c("Jon Snow", 
-                                 "Tyrion Lannister", 
-                                 "Daenerys Targaryen", 
-                                 "Sansa Stark", 
-                                 "Cersei Lannister", 
-                                 "Arya Stark", 
-                                 "Jaime Lannister", 
-                                 "Samwell Tarly", 
-                                 "None"), 
-                     selected = "None")
-      ),
-
       # Show a plot of the generated distribution
-      mainPanel(
-         h2 ("Screen Time Per Season"),
-         
-         plotOutput("distPlot"),
-         
-         
-         h2("Character 1 Information"),
-         imageOutput("image1"),
-         textOutput("Character1TimePerEpisode"),
-         conditionalPanel(condition = "input.Character2 != 'None'",
-                          p(h2("Character 2 Information"))),
-         conditionalPanel(condition = "input.Character2 != 'None'",p(imageOutput("image2")))                 
+        fluidRow(
+           column (3,offset = 1,style = 'margin-top:3%;',
+                    #h4( "Select Characters"),
+                    selectInput("Character",
+                                  label = "Choose Character to display their Screen Time",
+                                  choices = c("Jon Snow", 
+                                              "Tyrion Lannister", 
+                                              "Daenerys Targaryen", 
+                                              "Sansa Stark", 
+                                              "Cersei Lannister", 
+                                              "Arya Stark", 
+                                              "Jaime Lannister", 
+                                              "Samwell Tarly"), 
+                                  selected = "Jon Snow"),
+                      selectInput("Character2",
+                                  label = "Choose Character to display their Screen Time",
+                                  choices = c("None",
+                                              "Jon Snow", 
+                                              "Tyrion Lannister", 
+                                              "Daenerys Targaryen", 
+                                              "Sansa Stark", 
+                                              "Cersei Lannister", 
+                                              "Arya Stark", 
+                                              "Jaime Lannister", 
+                                              "Samwell Tarly" 
+                                              ), 
+                                  selected = "None")
+                    ), 
+            column(7, offset = .75,
+                   h2("Screen Time Per Season"),
+                   plotOutput("distPlot")
+                   )        
+
+         ),
+        div(style = 'margin-top: -10%;',
+        fluidRow(
+               column(7, offset = 1,
+               h2("Character 1 Information")
+        )
+          )
+        ),
+        div(style = 'margin-top: 0%;',
+        fluidRow(
+          column(2, offset =1,
+                 h4("House Sigil:"),
+                 imageOutput("image1")
+                 ), 
+          
+          column(3,
+                 h4( "The Average Screen Time Per Episode"),
+                 textOutput("Character1TimePerEpisode"), 
+                 tags$style("#Character1TimePerEpisode {font-size:50px;color:white;}")
+                 ),
+          
+          column(3, 
+                 h4( "Goodness Score:"),
+                 textOutput("Character1Good"), 
+                 tags$style("#Character1Good {font-size:50px;color:white;}")
+                ), 
+          column(3,
+                 h4( "Kill Count:")
+                 #textOutput("Character1KillCount"),
+                 #tags$style("#Character1KillCount {font-size:50px;color:white;}")
+                )
+          
+
+        )),
+  
+        
+        conditionalPanel(
+          condition = "input.Character2 != 'None' && input.Character2 != input.Character1",
+          style = 'margin-top: -10%;',
+          fluidRow(
+            column(7, offset = 1,
+                   h2("Character 2 Information")
+            )
+          )
+          ),
+          fluidRow(
+          column(2, offset=1,
+                 h4("House Sigil:"),
+                 imageOutput("image2")
+          ),
+          column(3,
+                 h4( "The Average Screen Time Per Episode"),
+                 textOutput("Character2TimePerEpisode"),
+                 tags$style("#Character2TimePerEpisode {font-size:50px;color:white;}")
+          ),
+
+          column(3,
+                 h4( "Goodness Score:"),
+                 textOutput("Character2Good"),
+                 tags$style("#Character2Good {font-size:50px;color:white;}")
+          ),
+          column(3,
+                 h4( "Kill Count:")
+                 #textOutput("Character1KillCount"),
+                 #tags$style("#Character1KillCount {font-size:50px;color:white;}")
+          )
+        )  
       )
-   )
-)
+
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -87,6 +150,8 @@ server <- function(input, output) {
      ggplot(df, aes(variable, value, group = character, color = character))+
        geom_line(lwd = 2) + scale_color_manual(values = characters.colors) + theme_economist()
    })
+   
+   
    output$image1 = renderImage({ 
      if (input$Character %in% starks)
      {
@@ -96,9 +161,11 @@ server <- function(input, output) {
      {
        img = "~/Documents/GitHub/GOT/GameofThrones/lannisterSigil.png"
      }
-     list(src = img, width = 200,
-          height = 250)},
+     list(src = img, width = 175,
+          height = 200)},
      deleteFile = FALSE)
+   
+   
    output$image2 = renderImage({ 
      if (input$Character2 %in% starks)
      {
@@ -108,14 +175,31 @@ server <- function(input, output) {
      {
        img = "~/Documents/GitHub/GOT/GameofThrones/lannisterSigil.png"
      }
-     list(src = img, width = 200,
-          height = 250)},
+     list(src = img, width = 175,
+          height = 200)},
      deleteFile = FALSE)
+   
+   
    output$Character1TimePerEpisode = renderText ({
      characterTime =top10TimePerEpisode %>% filter(character == input$Character)
-     paste("The Average Screen Time Per Episode is: ", characterTime$timePerEp)
+     paste(round(characterTime$timePerEp,2))
    })
-
+   
+   output$Character2TimePerEpisode = renderText ({
+     characterTime2 =top10TimePerEpisode %>% filter(character == input$Character2)
+     paste(round(characterTime2$timePerEp,2))
+   })
+   
+   output$Character1Good = renderText ({
+     characterGood =ScreenTime1 %>% filter(character == input$Character) %>%summarize(Goodness = mean(Goodness))
+     paste(round(characterGood$Goodness,2))
+   })
+   
+   output$Character2Good = renderText ({
+     character2Good =ScreenTime1 %>% filter(character == input$Character2) %>%summarize(Goodness = mean(Goodness))
+     paste(round(character2Good$Goodness,2))
+   }) 
+   
 }
 
 
